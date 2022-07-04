b8b573c02cb98ee39bbb0fdec6620ad74b17131d
saurabhp199 <aditi.patil@logibricks.in>
2022-07-05 02:33:22 +0530

    updated tfstate config

--- version.tf
+++ version.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.0" 
-    }
-  }
+	backend "azurerm" {
+	  resource_group_name  = "bhs-tfstate"
+	  storage_account_name = "bhstf"
+	  container_name       = "bhsdevops"
+	  key                  = "terraformbhs.tfstate"
+	}
 }
 
-# Provider Block
 provider "azurerm" {
- features {}          
+  version = "~>2.0"
+  features {}
 }
+ 
