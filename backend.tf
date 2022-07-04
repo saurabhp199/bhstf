ed6ca2992efc0886803f4987001c72b7b05c6bf9
saurabhp199 <aditi.patil@logibricks.in>
2022-07-05 02:24:14 +0530

    removed backend config

--- backend.tf
+++
@@ -1,6 +0,0 @@
-backend "azurerm" {
-  resource_group_name  = "bhs-tfstate"
-  storage_account_name = "bhstf"
-  container_name       = "bhsdevops"
-  key                  = "terraformbhs.tfstate"
-}
