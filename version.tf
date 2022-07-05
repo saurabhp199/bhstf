5c4b62666ab72d0f17b8639b845a9f4945f83fd2
saurabhp199 <aditi.patil@logibricks.in>
2022-07-05 09:24:04 +0530

    changed terraform yml config

--- version.tf
+++ version.tf
@@ -1,14 +1,13 @@
 terraform {
-	backend "azurerm" {
-	  resource_group_name  = "bhs-tfstate"
-	  storage_account_name = "bhstf"
-	  container_name       = "bhsdevops"
-	  key                  = "terraformbhs.tfstate"
-	}
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" 
+    }
+  }
 }
-
+# Provider Block
 provider "azurerm" {
-  version = "~>2.0"
-  features {}
+ features {}          
 }
- 
--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "myvnet" {
-  name                = "bhsvnet-1"
+  name                = "bhsvnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "myvnet" {
 
 # Create Subnet
 resource "azurerm_subnet" "mysubnet" {
-  name                 = "bhssubnet-1"
+  name                 = "bhssubnet"
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.2.0/24"]
